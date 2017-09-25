b28d53dddd6b1849c2f0c88c97995d53b92e572b
Olivier Butler <olivier.butler@gmail.com>
2017-09-25 15:43:42 -0700

    Basic QR code detection and position build

--- iOSTraining-PirateDrawing/ViewController.swift
+++ iOSTraining-PirateDrawing/ViewController.swift
@@ -9,10 +9,54 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var qRRequest:VNDetectBarcodesRequest?
+    var qRTimer = Timer()
+    var testCounterForTimer:Int = 0
+    
+    /************************/
+    /* The QR Functionality */
+    /************************/
+    // Runs a detection with the current image from the sceneView
+    @objc func detectQR(){
+        let cameraCurrent = sceneView.session.currentFrame?.capturedImage
+        let handler = VNImageRequestHandler(cvPixelBuffer: cameraCurrent!, options: [.properties : ""])
+        // Perform the barcode-request. This will call the completion-handler of the barcode-request.
+        guard let _ = try? handler.perform([qRRequest!]) else {
+            return print("Could not perform barcode-request!")
+        }
+    }
+    // Setup for a barcode detector
+    func setupCodeRequest(){
+        qRRequest = VNDetectBarcodesRequest(completionHandler: {(request, error) in
+            // Loopm through the found results
+            for result in request.results! {
+                
+                // Cast the result to a barcode-observation
+                if let barcode = result as? VNBarcodeObservation {
+                    // Print barcode-values
+                    print("Payload: \(barcode.payloadStringValue!)")
+                    
+                    // Get the bounding box for the bar code and find the center
+                    var rect = barcode.boundingBox
+                    // Flip coordinates
+                    rect = rect.applying(CGAffineTransform(scaleX: 1, y: -1))
+                    rect = rect.applying(CGAffineTransform(translationX: 0, y: 1))
+                    let center = CGPoint(x: rect.midX, y: rect.midY)
+                    print ("Payload: \(barcode.payloadStringValue!) at \(center)")
+                }
+            }
+        })
+    }
+    // Starts a timer with a callback
+    func scheduledTimerWithTimeInterval(){
+        // Scheduling timer to Call the function "updateCounting" with the interval of 1 seconds
+        qRTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.detectQR), userInfo: nil, repeats: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +67,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        print("Loaded and empty")
+        setupCodeRequest()
+        scheduledTimerWithTimeInterval()
         
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -51,30 +94,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
